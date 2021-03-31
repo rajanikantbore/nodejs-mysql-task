d34d3d75703ec3b33e8a03d9301555babc7b1f94
Rajanikant Bore <74585466+rajanikantbore@users.noreply.github.com>
2021-03-31 09:57:16 +0530

    recent task

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let mysql = require('mysql');
 let express = require('express');
 let app = express();
-let bodyparser = require('body-parser');
-app.use(bodyparser.json);
+app.use(express.urlencoded({ extended: false }))
+const util = require('util')
+
+// parse application/json
+app.use(express.json())
 
 
 let mysqlConnection = mysql.createConnection({
@@ -13,46 +16,65 @@ let mysqlConnection = mysql.createConnection({
     database: "coursesdb"
 });
 
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('DB connected Successfully');
-    else
-        console.log('DB connction failed \n Error :' + JSON.stringify(err, undefined, 2));
-
-});
-
-app.listen(3200, () => console.log('Express server runnning on port no: 3200'));
-
-
-app.get('/fetch', (req, res) => {
-    mysqlConnection.query('SELECT * FROM coursesdb.classroom', (err, records) => {
-        if (err) throw err;
-        else
-            res.send(records);
-    })
+app.get('/classes/:classId', (req, res) => {
+    try {
+        const classId = req.params.classId;
+        console.log(classId)
+        mysqlConnection.query(`SELECT 
+    StudentName, SubjectName, StaffName
+FROM
+	coursesdb.classroom cls
+    join coursesdb.students std on std.classID = cls.classID
+    join coursesdb.subject sub on sub.classID = cls.classID
+    join coursesdb.faculty fac on fac.classId = cls.classID
+WHERE cls.classID =${classId};`, (err, records) => {
+            if (err) throw err;
+            else
+                res.send(records);
+        })
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 //update
-app.put('/update', (req, res) => {
+app.put('/staff/:staffId', (req, res) => {
+    const staffId = req.params.staffId;
+    const { staffName } = req.body;
+    // TODO check staff exists
+    // TODO if not exist send response 400 with message bad request or staff not found
+    // TODO If exist update the record and fetch the record and send it in response
     mysqlConnection.query(`update coursesdb.faculty set StaffName = '${req.body.StaffName}' where 
         StaffID =${req.body.StaffID}`, (err, result) => {
-        if (err) throw err;
-        else {
+        if (err) {
+            res.status(500).send({ message: 'Failed to update the staff details due to techical difficulties!!' });
+        } else {
             res.send({ update: "success" });
         }
     })
 });
 
 //insert
-app.post("/insert", (req, res) => {
-    mysqlconnection.query(`insert into coursesdb.subject values(
-                            ${req.body.SubID},
-                            ${req.body.SubjectName},
-                            ${req.body.classID})`, (err, result) => {
-        if (err) throw err;
+app.post("/subjects", (req, res) => {
+    // TODO check duplicates
+    mysqlconnection.query(`insert into coursesdb.subject(SubjectName) values(
+                            ${req.body.SubjectName}`, (err, result) => {
+        if (err) throw err; //chage
         else {
             res.send({ insert: "success" });
         }
     })
 });
 
+mysqlConnection.connect((err) => {
+    if (err) {
+        console.log('DB connction failed \n Error :' + util.inspect(err, {
+            depth: 5,
+        }));
+        process.exit(1)
+    } else {
+        console.log('DB connected Successfully');
+        app.listen(3200, () => console.log('Express server runnning on port no: 3200'));
+    }
+
+});
